cfda60e73e15d0af75f31122c156c6853ee17637
Danail Gatev <danailgatev11@gmail.com>
2025-10-28 21:11:07 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,26 @@
+function toggleDropdown() {
+	const dropdown = document.getElementById("myDropdown");
+	const button = document.getElementById("menuBtn");
+
+	dropdown.classList.toggle("show");
+	button.classList.toggle("active");
+}
+
+window.addEventListener(
+
+"click",
+function(event) {
+	const dropdown = document.getElementById("myDropdown");
+	const button = document.getElementById("menuBtn");
+
+	if (dropdown.contains(event.target) || button.contains(event.target)) {
+		return;
+	}
+	
+	if (dropdown.classList.contains("show")) {
+		dropdown.classList.remove("show");
+		button.classList.remove("active");
+	}
+}
+)
+;	
