ad86be0f1f75bd24b9dd440c40ab4b973f77d9fd
Danail Gatev <danailgatev11@gmail.com>
2026-02-09 12:48:42 +0200

    scripts.js

--- scripts.js
+++ scripts.js
@@ -1,26 +1,26 @@
 function toggleDropdown() {
-	const dropdown = document.getElementById("myDropdown");
-	const button = document.getElementById("menuBtn");
+const dropdown = document.getElementById("myDropdown");
+const button = document.getElementById("menuBtn");
 
-	dropdown.classList.toggle("show");
-	button.classList.toggle("active");
+dropdown.classList.toggle("show");
+button.classList.toggle("active");
 }
 
 window.addEventListener(
 
 "click",
 function(event) {
-	const dropdown = document.getElementById("myDropdown");
-	const button = document.getElementById("menuBtn");
+const dropdown = document.getElementById("myDropdown");
+const button = document.getElementById("menuBtn");
 
-	if (dropdown.contains(event.target) || button.contains(event.target)) {
-		return;
-	}
-	
-	if (dropdown.classList.contains("show")) {
-		dropdown.classList.remove("show");
-		button.classList.remove("active");
-	}
+if (dropdown.contains(event.target) || button.contains(event.target)) {
+return;
+}
+
+if (dropdown.classList.contains("show")) {
+dropdown.classList.remove("show");
+button.classList.remove("active");
+}
 }
 )
-;	
+;
